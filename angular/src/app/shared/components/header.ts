83bc35d4aabe383b58f58f822ef2fdbe783cc372
JoeJoeJoe352 <sez520@gmail.com>
2026-08-18 15:34:55 +0200

    header egységesítés

--- angular/src/app/shared/components/header.ts
+++ angular/src/app/shared/components/header.ts
@@ -4,50 +4,64 @@ import { Router, RouterLink } from '@angular/router';
 import { UserDataStore } from '../services/user-data-store';
 import { TranslatePipe } from '@ngx-translate/core';
 
+interface LinkInterface {
+    url: string;
+    langKey: string;
+}
+
 @Component({
-  selector: 'app-header',
-  templateUrl: 'header.html',
-  styleUrl: './header.scss',
-  imports: [RouterLink, TranslatePipe],
+    selector: 'app-header',
+    templateUrl: 'header.html',
+    styleUrl: './header.scss',
+    imports: [RouterLink, TranslatePipe],
 })
 export class Header {
-  private authService = inject(AuthService);
-  private router = inject(Router);
-  protected userData = inject(UserDataStore)
+    private authService = inject(AuthService);
+    private router = inject(Router);
+    protected userData = inject(UserDataStore);
 
-  /**
-   * Hamburger menu lenyitása
-   */
-  protected menuOpen = false;
-  
-  /**
-   * Esc gomb listeren, mi történjen, ha esc-t nyom a user
-   */
-  @HostListener('document:keydown.escape')
-  onEsc() {
-    this.menuOpen = false;
-  }
+    /**
+     * Hamburger menu lenyitása
+     */
+    protected menuOpen = false;
 
-  /**
-   * hamburger menu lenyitása/bezárása
-   */
-  toggleMenu(): void {
-    this.menuOpen = !this.menuOpen;
-  }
+    protected publicLinkList: LinkInterface[] = [
+        { url: '/', langKey: 'header.dashboard' },
+        { url: '/welcome', langKey: 'header.welcome' },
+    ];
 
-  /**
-   * Felhasználó kijelentkeztetése
-   */
-  logout(): void {
-    this.authService.logout().subscribe({
-      next: () => {
-        this.userData.resetData()
-        this.router.navigate(["/welcome"]) // ez az utolsó mindig
-      },
-      error: (response) => {
-        console.error(response)
-        //TODO: új toast: this.snackbar.open('Error during logout', 'close');
-      }
-    });
-  }
+    protected authenticatedLinkList: LinkInterface[] = [
+        {url: "/transactions", langKey: "header.transactions"}
+    ];
+
+    /**
+     * Esc gomb listeren, mi történjen, ha esc-t nyom a user
+     */
+    @HostListener('document:keydown.escape')
+    onEsc() {
+        this.menuOpen = false;
+    }
+
+    /**
+     * hamburger menu lenyitása/bezárása
+     */
+    toggleMenu(): void {
+        this.menuOpen = !this.menuOpen;
+    }
+
+    /**
+     * Felhasználó kijelentkeztetése
+     */
+    logout(): void {
+        this.authService.logout().subscribe({
+            next: () => {
+                this.userData.resetData();
+                this.router.navigate(['/welcome']); // ez az utolsó mindig
+            },
+            error: (response) => {
+                console.error(response);
+                //TODO: új toast: this.snackbar.open('Error during logout', 'close');
+            },
+        });
+    }
 }
