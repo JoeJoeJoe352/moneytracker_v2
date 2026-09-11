33c59e3f50ebb7eec615ea34a476ed0f7174efa2
JoeJoeJoe352 <sez520@gmail.com>
2026-09-11 16:45:27 +0200

    sidebar refakt

--- angular/src/app/app.ts
+++ angular/src/app/app.ts
@@ -1,18 +1,18 @@
 import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/components/header';
-import { Footer } from './shared/components/footer';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core';
 import { SUPPORTED_LANGS } from './shared/utils/language-util';
 import { SupportedLangEnum } from './shared/enums';
 import { LanguageService, LOCALSTORAGE_KEY_LANG } from './shared/services/translate-service';
+import { Sidebar } from './shared/components/sidebar';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, Header, Footer, ReactiveFormsModule],
     templateUrl: './app.html',
     styleUrl: './app.scss',
+    imports: [RouterOutlet, Header, ReactiveFormsModule, Sidebar],
 })
 export class App {
     protected readonly title = signal('Moneytracker');
---
+++ angular/src/app/shared/components/sidebar.ts
@@ -0,0 +1,84 @@
+import { Component, HostListener, inject } from '@angular/core';
+import { AuthService } from '../../features/auth/auth-service';
+import { Router, RouterLink } from '@angular/router';
+import { UserDataStore } from '../services/user-data-store';
+import { LinkInterface } from '../interfaces';
+import { TranslatePipe } from '@ngx-translate/core';
+import { HeaderLinkListComponent } from './header-link-list-component';
+import { LanguageSwitcherComponent } from './language-switch-component';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatButton, MatIconButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+    selector: 'app-sidebar',
+    templateUrl: 'sidebar.html',
+    styleUrl: './sidebar.scss',
+    imports: [
+        RouterLink,
+        TranslatePipe,
+        HeaderLinkListComponent,
+        LanguageSwitcherComponent,
+        MatToolbar,
+        MatButton,
+        MatIconButton,
+        MatIcon,
+    ],
+})
+export class Sidebar {
+    private authService = inject(AuthService);
+    private router = inject(Router);
+    protected userData = inject(UserDataStore);
+
+    /**
+     * Hamburger menu lenyitása
+     */
+    protected menuOpen = false;
+
+    /**
+     * Publikusan elérhető linkek listája a fejlécben
+     */
+    protected publicLinkList: LinkInterface[] = [
+        { url: '/', langKey: 'header.dashboard' },
+        { url: '/welcome', langKey: 'header.welcome' },
+    ];
+
+    /**
+     * Belépés után elérhető linkek listája a fejlécben
+     */
+    protected authenticatedLinkList: LinkInterface[] = [
+        { url: '/transactions', langKey: 'header.transactions' },
+        { url: '/wallets', langKey: 'header.wallets' },
+    ];
+
+    /**
+     * Esc gomb listeren, mi történjen, ha esc-t nyom a user
+     */
+    @HostListener('document:keydown.escape')
+    onEsc() {
+        this.menuOpen = false;
+    }
+
+    /**
+     * hamburger menu lenyitása/bezárása
+     */
+    toggleMenu(): void {
+        this.menuOpen = !this.menuOpen;
+    }
+
+    /**
+     * Felhasználó kijelentkeztetése
+     */
+    logout(): void {
+        this.authService.logout().subscribe({
+            next: () => {
+                this.userData.resetData();
+                this.router.navigate(['/welcome']); // ez az utolsó mindig
+            },
+            error: (response) => {
+                console.error(response);
+                //TODO: új toast: this.snackbar.open('Error during logout', 'close');
+            },
+        });
+    }
+}
