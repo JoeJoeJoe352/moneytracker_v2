edfa75dc86072b36a027a813075255ddf6d332b4
JoeJoeJoe352 <sez520@gmail.com>
2026-09-23 08:28:31 +0200

    error page oldal

--- angular/src/app/features/error/error-page-component.ts
+++ angular/src/app/features/error/error-page-component.ts
@@ -1,12 +1,27 @@
-import { Component } from "@angular/core";
-import { TranslatePipe } from "@ngx-translate/core";
+import { AfterViewInit, Component, ElementRef, inject, viewChild } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { Router } from '@angular/router';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
-    selector: "app-error-component",
-    template: '<h1>{{ "error.not_found" | translate }}</h1>',
+    selector: 'app-error-component',
+    templateUrl: './error-page-component.html',
+    styleUrl: './error-page-component.scss',
     standalone: true,
-    imports: [TranslatePipe],
+    imports: [TranslatePipe, MatCardModule, MatButtonModule],
 })
-export class ErrorPageComponent {
+export class ErrorPageComponent implements AfterViewInit {
+    private readonly router = inject(Router);
+    private readonly backToHomepageButton = viewChild.required('mainPageButton', {
+        read: ElementRef<HTMLButtonElement>,
+    });
 
+    ngAfterViewInit(): void {
+        this.backToHomepageButton().nativeElement.focus();
+    }
+
+    protected redirectToMainPage() {
+        return this.router.navigate(['/']);
+    }
 }
